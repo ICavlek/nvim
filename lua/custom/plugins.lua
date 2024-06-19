5c73f8c61062cfbf7d72c725ac52c1e7f49e7171
ICavlek <ivan.cavlek1403@gmail.com>
2024-06-19 12:35:46 +0200

    Delete rust-analyzer in favour of rust-tools

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -20,6 +20,17 @@ local plugins = {
     init = function()
       vim.g.rustfmt_autosave = 1
     end
-  }
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require('rust-tools').setup(opts)
+    end
+  },
 }
 return plugins
