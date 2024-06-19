65c470508a534c0ffc0be6980e2b1abc11f707e5
ICavlek <ivan.cavlek1403@gmail.com>
2024-06-19 12:53:29 +0200

    Added crates versioning

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -34,6 +34,23 @@ local plugins = {
   },
   {
     "mfussenegger/nvim-dap",
+  },
+  {
+    'saecki/crates.nvim',
+    ft = {"rust", "toml"},
+    config = function(_, opts)
+      local crates = require('crates')
+      crates.setup(opts)
+      crates.show()
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
   }
 }
 return plugins
