0f9af817c328e6552005495ee53118bdf2a70245
ICavlek <ivan.cavlek1403@gmail.com>
2025-10-18 09:50:37 +0200

    fix: async trait and warning on LspRestart, it properly does now

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -2,7 +2,30 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
-      rust_analyzer = {},
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            procMacro = {
+              enable = true,
+              attributes = {
+                enable = true,
+              },
+              ignored = {
+                ["async-recursion"] = { "async_recursion" },
+                ["napi-derive"] = { "napi" },
+              },
+            },
+          },
+        },
+      },
     },
   },
+  init = function()
+    -- Register the LSP config for nvim 0.11+
+    vim.lsp.config("rust-analyzer", {
+      cmd = { "rust-analyzer" },
+      filetypes = { "rust" },
+      root_markers = { "Cargo.toml" },
+    })
+  end,
 }
